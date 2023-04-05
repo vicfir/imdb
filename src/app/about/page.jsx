484ee563875481de0c26e181271cd41a613ff846
vicfir <vicfir@hotmail.fr>
2023-04-05 17:25:39 +0200

    page about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+        <h1 className="text-2xl font-medium text-amber-500">About</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates nesciunt aliquam nam totam ipsam molestias. Non quo dolor nostrum assumenda officia dolore explicabo incidunt perspiciatis. Eveniet minima libero itaque porro?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque autem ea tempora ullam aperiam molestiae debitis doloribus voluptatum blanditiis cum quod minus rerum repellat ex repudiandae sit veniam sequi quasi error dolor nemo laboriosam, corporis possimus! Cum, eaque. Vero, iure. Minus nostrum, molestiae facere fugit natus libero inventore temporibus maxime alias nihil neque non eaque, quia pariatur autem culpa quasi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis officia similique dolore, distinctio maxime culpa? Tempora ut aliquam corporis dolores. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure nam distinctio obcaecati, dignissimos delectus ex doloremque, repellat cupiditate ab numquam totam! Quasi quis corporis, voluptatem quisquam mollitia accusantium maiores impedit! Odio blanditiis voluptates explicabo maiores? Ex harum dolorum provident vel! Similique placeat est illo esse aspernatur debitis perferendis, neque laboriosam.</p>
+    </div>
+  )
+}
